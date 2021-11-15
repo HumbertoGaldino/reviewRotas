88638ae5b4a996295b7532162bdbad2363fc948e
Humberto <humberto.galdino@live.com>
2021-11-15 18:05:22 -0300

    Adiciona Rotas

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # reviewRotas
 Revisão do conteúdo de rotas de um servidor HTTP com Node.js. Conteúdo do curso Web Fullstack da Digital House.
+
+>Através do sistema de rotas do Express, podemos definir como nossa aplicação irá responder segundo o método HTTP e a rota que está chegando ao servidor.
--- app.js
+++ app.js
@@ -6,3 +6,7 @@ let app = express();
 app.listen(3000,()=>console.log("Servidor rodando na porta 3000"));
 
 app.get('/',(req,res)=>res.send("Olá mundo!"));
+
+app.get('/contato', (req,res)=>res.send(['Contato1','Contato2']));
+
+app.get('/endereco', (req,res)=>res.send({endereco:'Rua dos Alfeneiros', numero:4,cidade:"Little Whinging",complemento:"Condado de Surrey"}));
